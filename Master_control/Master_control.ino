8f7624af551742de765aebda2db98e1e596eafa8
Samuel Findling <48837184+finds2605@users.noreply.github.com>
2019-05-22 12:37:52 -0400

    Add files via upload
    Code for Arduino master currently only uses the button matrix

---
+++ Master_control/Master_control.ino
@@ -0,0 +1,64 @@
+
+bool Pressed=false;
+int first=0;
+bool Readable[8]={true,true,true,true,true,true,true,true};
+int  PINS [8]={4,5,6,7,10,11,12,13};
+int Line=0;
+int Press=0;
+void setup() {
+  pinMode(4,INPUT_PULLUP);
+  pinMode(5,INPUT_PULLUP);
+  pinMode(6,INPUT_PULLUP);
+  pinMode(7,INPUT_PULLUP);
+  pinMode(13,INPUT_PULLUP);
+  pinMode(12,INPUT_PULLUP);
+  pinMode(11,INPUT_PULLUP);
+  pinMode(10,INPUT_PULLUP);
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  
+if (Pressed){
+  Serial.println(Press);
+}
+else{
+  
+    Press=8;
+  for (int X=0; X<8; X++){
+    int J=Read(PINS[X]);
+    if (J){
+      Press=X;
+      }
+  }
+  if (bitRead(Line,Press)){
+    Pressed=true;
+      first=Press;
+     Write(Press,true);
+     Serial.println(first);
+
+  }
+  }
+if (Serial.available()){
+ Line=Serial.read();
+Pressed=false;
+}
+
+
+  
+  // put your main code here, to run repeatedly:
+
+//to determin the first one pressed
+ 
+
+
+}
+void Write(int Slave, bool State){
+  
+}
+int Read(int Num){
+ return(!digitalRead(Num));
+ 
+}
+
