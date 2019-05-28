d62875a061db1e77eaa00e0d331643b7c12544e0
Zane Hancock <49283734+ZJHancock@users.noreply.github.com>
2019-05-28 12:02:32 -0400

    Added gui.update_state(state_str)
    Used to change which text fields are on the screen (in response to events from the mouse clicks).  The same function will also used when button presses is received from the Arduino.

--- Game.py
+++ Game.py
@@ -1,8 +1,9 @@
 from GUI import *
 
+#################### INIT ####################
 gui = GraphicalUserInterface(600, 450, WHITE)
 
-gui.start_new_question('OpenTopic', 'ReaderAsking')
+gui.start_new_question('OpenTopic')
 ## Update all the relevant text fields
 
 #### MAIN ####
@@ -13,8 +14,29 @@ while True:
     gui.check_events()
     
     if gui.user_mouse_input:
-        print(gui.user_mouse_input)
-        ## Probably handle the event first
+
+        ##print(gui.user_mouse_input)
+        
+        if gui.user_mouse_input == 'ShowAnswer':
+            gui.update_state('NoPointsShowAnswer')
+
+        elif gui.user_mouse_input == 'Correct':
+            # Record points
+            pass
+            # Start the next question
+            gui.start_new_question('OpenTopic')
+
+        elif gui.user_mouse_input == 'Incorrect':
+            if True: # someone else is eligble to answer the question
+                gui.update_state('ReaderAsking')
+            else: # No one is eligble, show the answer
+                gui.update_state('NoPointsShowAnswer')
+
+        elif gui.user_mouse_input == 'NextQuestion':
+            # Start the next qestion...
+            gui.start_new_question('OpenTopic')
+
+        # Reset the GUI's mouse input attribute
         gui.user_mouse_input = None
     
     gui.render()
