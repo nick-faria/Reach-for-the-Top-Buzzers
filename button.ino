13a8f196497fb581b82209887b2848c62b550562
Trevor Last <trevorclast@gmail.com>
2019-06-19 14:11:37 -0400

    Add files via upload

---
+++ button.ino
@@ -0,0 +1,386 @@
+/*
+ *                            THE BUTTONS
+ *                            ===========
+ *
+ *   There are 8 buttons. Each button is tied to a certain team, where
+ * each team can have 4 buttons. The buttons each have an LED and a
+ * piezo buzzer, which are controlled by the server. When the button is
+ * pressed, it will send a message to the server. After the button has
+ * been pressed, it will not send another event to the server until it
+ * receives a "new question" message from the server.
+ *
+ */
+/* TODO:
+ */
+
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+
+#include <button_radio.h>
+
+
+/* ==[ VARIABLES ]== */
+/* Pins */
+enum
+{
+  PIN_CE     = 7,
+  PIN_CSN    = 6,
+
+  PIN_LIGHT  = 9,
+  PIN_SOUND  = 5,
+
+  PIN_BUTTON = 8,
+};
+
+
+RF24 radio(PIN_CE, PIN_CSN);
+RF24Network network(radio);
+
+uint16_t       this_node = 01;
+uint16_t const parent    = 00;
+
+uint8_t this_team = 0;
+
+
+bool can_be_pressed  = true;
+
+int  button_previous = HIGH;
+bool button_is_held = false;
+unsigned long button_pressed_time = 0;
+unsigned long const PAIRING_MODE_START_DELAY = 1000;
+
+byte registers[REGISTER_COUNT];
+
+
+/* ==[ FUNCTIONS ]== */
+void interpret_message(RF24NetworkHeader header, byte *message);
+bool pair(void);
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+  radio.begin();
+  radio.setDataRate(RF24_2MBPS);
+
+  network.multicastLevel(this_team);
+
+  pinMode(PIN_LIGHT , OUTPUT);
+  pinMode(PIN_BUTTON, INPUT_PULLUP);
+
+  debug("Initialized");
+}
+
+void loop()
+{
+  debug("loop");
+  network.update();
+
+  /* Receive messages */
+  while (network.available())
+  {
+    /* Read the message from the network */
+    RF24NetworkHeader header;
+    char message[32];
+    debug("Reading...");
+    network.read(header, &message, 32);
+    debug("Received '%s'", message);
+
+    interpret_message(header, message);
+  }
+
+  /* hHIf it is held for long enough, the unit will enter pairing mode */
+  int button_state = digitalRead(PIN_BUTTON);
+  if (button_state != button_previous)
+  {
+    debug("button pushed");
+
+    /* The button was pressed */
+    if (button_state == LOW)
+    {
+      button_pressed_time = millis();
+      button_is_held = true;
+
+      /* Send a button press event to the server */
+      if (can_be_pressed)
+      {
+        can_be_pressed = false;
+        RF24NetworkHeader header(parent, ID_BUTTON_EVENT);
+    
+        ButtonEvent btn_event(this_team, this_node);
+        int msg_size = 0;
+        byte *msg = btn_event.to_message(&msg_size);
+    
+        network.write(header, msg, msg_size);
+        delete[] msg;
+        debug("button event");
+      }
+    }
+    else
+    {
+      button_is_held = false;
+    }
+  }
+  button_previous = button_state;
+
+  /* After the button has been held for a certain amount of time, attempt to pair */
+  if (button_is_held && millis() - button_pressed_time >= PAIRING_MODE_START_DELAY)
+  {
+    /* Hold our previous address, so we can reuse
+     * our old address if we fail to pair */
+    uint16_t previous_address = this_node;
+    uint8_t  previous_team    = this_team;
+    uint8_t  previous_led     = registers[REG_LIGHT];
+    if (!pair())
+    {
+      debug("Failed to pair!");
+      this_node = previous_address;
+      this_team = previous_team;
+      network.begin(this_node);
+      network.multicastLevel(this_team);
+
+      registers[REG_LIGHT] = previous_led;
+    }
+    else
+    {
+      button_is_held = false;
+      can_be_pressed = true;
+
+      /* We're connected, so LED is on */
+      registers[REG_LIGHT] = 0xFF;
+    }
+  }
+
+  /* Update the registers */
+  analogWrite(PIN_LIGHT, registers[REG_LIGHT]);
+
+  /* The buzzer is only on while SOUND_TIMER is greater than 0 */
+  if (registers[REG_SOUND_TIMER] > 0x00)
+  {
+    if (registers[REG_SOUND] == 0x00)
+    {
+      noTone(PIN_SOUND);
+    }
+    else
+    {
+      int pitch = map(registers[REG_SOUND], 0, 255, 512, 1024);
+      tone(PIN_SOUND, pitch);
+    }
+
+    registers[REG_SOUND_TIMER]--;
+  }
+  /* Once the SOUND_TIMER runs out, turn off the buzzer */
+  else
+  {
+    noTone(PIN_SOUND);
+  }
+
+  delay(50);
+}
+
+
+
+/* intepret_message: interpret a message from the network */
+void interpret_message(RF24NetworkHeader header, byte *message)
+{
+  switch(header.type)
+  {
+  /* Instruction */
+  case ID_INSTRUCTION:
+   {
+    byte opcode = message[1];
+    switch (opcode)
+    {
+    /* Mark a new question */
+    case OP_NEW_QUESTION:
+     {
+      debug("New Question");
+      can_be_pressed = true;
+      registers[REG_LIGHT] = 0xFF;
+     }break;
+
+    /* Write to a register */
+    case OP_WRITE_REGISTER:
+     {byte reg   = message[2],
+           value = message[3];
+
+      debug("write 0x%X to register 0x%X", value, reg);
+
+      registers[reg] = value;
+     }break;
+    
+    default:
+      error("unknown opcode %.2hhX", opcode);
+      break;
+    }
+   }break;
+
+  /* Disconnect message -- button is disabled for this question */
+  case ID_DISCONNECT:
+    can_be_pressed = false;
+    registers[REG_LIGHT] = 0x80;
+    debug("button is disabled");
+    break;
+
+  default:
+    error("unknown message ID 0x%.2hhX", header.type);
+    break;
+  }
+}
+
+/* pair: pair the button with the server */
+bool pair(void)
+{
+  /*  PAIRING PROCEDURE
+   *  =================
+   * connect to the server
+   * ping the server until it acknowledges or until it times out
+   * if we were acknowledged:
+   *  set our address to the address received from the server
+   *  ping the server again to verify our connection
+   *  if it failed:
+   *   goto begin.
+   *  else:
+   *    done.
+   * else:
+   *  goto begin.
+   */
+
+  /* Store our old name */
+  uint16_t old_name = this_node;
+
+  /* Node 01 is reserved for pairing */
+  network.begin(01);
+
+  /* Turn off the status LED; we're disconnected */
+  registers[REG_LIGHT] = 0x00;
+  digitalWrite(PIN_LIGHT, LOW);
+
+  /* Make sure there is a server to pair with */
+  if (ping())
+  {
+    debug("requesting new address!");
+
+    /* Request to pair with the server */
+    RF24NetworkHeader pair_header(parent, ID_PAIR);
+    char pair_msg[32] = "";
+    network.write(pair_header, pair_msg, sizeof(pair_msg));
+
+    /* Wait for the pairing message from the server */
+    bool got_pair_response = true;
+    unsigned long const PAIRING_TIMEOUT = 1000;
+    unsigned long pairing_begin = millis();
+    network.update();
+    while (!network.available())
+    {
+      debug("%lu", millis() - pairing_begin);
+      if (millis() - pairing_begin > PAIRING_TIMEOUT)
+      {
+        got_pair_response = false;
+        break;
+      }
+      network.update();
+    }
+
+    RF24NetworkHeader tmp_recv_header;
+    network.peek(tmp_recv_header);
+
+    if (got_pair_response && tmp_recv_header.type == ID_PAIR)
+    {
+      /* Receive our new address */
+      uint16_t address = 0;
+      uint8_t  team    = 0;
+      byte addr_data[sizeof(address) + sizeof(team)] = { 0 };
+
+      RF24NetworkHeader recv_header;
+      network.read(recv_header, &addr_data, sizeof(addr_data));
+
+      memcpy(&address, addr_data, sizeof(address));
+      memcpy(&team, addr_data + sizeof(address), sizeof(team));
+
+      debug("new address is %d on team %d", address, team);
+
+      /* Set our new address */
+      network.begin(address);
+      network.multicastLevel(team);
+  
+      /* Ensure we can connect to the server with our new address */
+      if (ping())
+      {
+        /* Send a disconnect message to the
+         * server, to free our old name */
+        RF24NetworkHeader discon_header(parent, ID_DISCONNECT);
+        byte discon_msg[32];
+        memcpy (discon_msg, &old_name, sizeof(old_name));
+        network.write(discon_header, discon_msg, sizeof(discon_msg));
+
+        this_node = address;
+        this_team = team;
+
+        /* Turn on the status LED; we're connected */
+        registers[REG_LIGHT] = 0xFF;
+
+        return true;
+      }
+      else
+      {
+        debug("Didn't receive pong after setting new address");
+      }
+    }
+    else if (tmp_recv_header.type == ID_DISCONNECT)
+    {
+      debug("Connection refused");
+      byte msg[32] = { 0 };
+      network.read(tmp_recv_header, &msg, sizeof(msg));
+    }
+    else
+    {
+      debug("New address message timed out");
+    }
+  }
+  /* We were not acknowledged */
+  else
+  {
+    debug("Server pong timed out!");
+  }
+
+  return false;
+}
+
+/* ping: ping the server */
+bool ping(void)
+{
+  unsigned long const PING_TIMEOUT = 1000;
+
+  /* Try to ping the server */
+  RF24NetworkHeader ping_header(parent, ID_PING);
+  char ping[32] = "ping";
+  network.write(ping_header, ping, sizeof(ping));
+  debug("Ping!");
+
+  unsigned long start = millis();
+
+
+  /* Wait for the pong from the server */
+  network.update();
+  while (!network.available())
+  {
+    network.update();
+    if (millis() - start > PING_TIMEOUT)
+    {
+      debug("ping() -- didn't receive a pong");
+      return false;
+    }
+  }
+
+  /* Receive the pong from the server */
+  RF24NetworkHeader pong_header;
+  char pong[32];
+  network.read(pong_header, &pong, sizeof(pong));
+
+  debug("ping() -- got a pong");
+  return true;
+}
+
---
+++ server.ino
@@ -0,0 +1,373 @@
+/*
+ *                            THE SERVER
+ *                            ==========
+ *
+ *   The server is the central hub. This is where all the buttons
+ * send their event data, and where the computer sends its
+ * instructions to the buttons. When the server receives an event
+ * message from the buttons, it passes it up to the computer.
+ * Likewise, when the computer sends instructions down to the server,
+ * the server will transmit them to the appropriate buttons.
+ *
+ */
+/* TODO:
+ *  - Get rid of the button disbled checking in BUTTON_EVENT
+ *    (Or maybe leave it in for redundancy?)
+ */
+
+#include <SPI.h>
+#include <RF24Network.h>
+#include <RF24.h>
+
+#include <button_radio.h>
+
+/* TODO: Set this to 1 when connected to the computer:
+ *       This disables the debug and error messages so they don't get
+ *       sent to the computer and screw stuff up */
+#if 1
+#define debug(fmt, ...) ({ })
+#define error(fmt, ...) ({ })
+#endif
+
+
+/* ==[ VARIABLES ]== */
+/* Pins */
+enum
+{
+  PIN_CE  = 7,
+  PIN_CSN = 8,
+
+  PIN_PAIR_BTN_TEAM_1 = 5,
+  PIN_PAIR_BTN_TEAM_2 = 4,
+};
+
+
+RF24 radio(PIN_CE, PIN_CSN);
+RF24Network network(radio);
+
+uint16_t const this_node = 00;
+
+size_t const MAXIMUM_CHILDREN = 4;
+uint16_t children[2][MAXIMUM_CHILDREN] = { { }, { } };
+unsigned child_count[2] = { 0, 0 };
+
+unsigned long const PAIRING_MODE_TIMEOUT = 10000;
+bool in_pairing_mode = false;
+unsigned long pairing_mode_start_time = 0;
+int pairing_team = 0;
+
+uint8_t button_mask = 0xFF;
+
+int pressed_button = -1;
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+  radio.begin();
+  network.begin(this_node);
+  radio.setDataRate(RF24_2MBPS);
+
+  pinMode(PIN_PAIR_BTN_TEAM_1, INPUT_PULLUP);
+  pinMode(PIN_PAIR_BTN_TEAM_2, INPUT_PULLUP);
+
+  debug("Initialized");
+}
+
+void loop()
+{
+  network.update();
+
+  /* Receive messages from the network */
+  while (network.available())
+  {
+    /* Read the message from the network */
+    RF24NetworkHeader header;
+    byte message[32];
+    network.read(header, &message, 32);
+    debug("Received '%s'", (char *)message);
+
+    interpret_message(header, message);
+  }
+
+  /* Receive messages from the computer; these are always just a
+   * button mask, and are only sent at the start of a new question */
+  while (Serial.available())
+  {
+    byte serial_msg = Serial.read();
+    button_mask = serial_msg;
+    pressed_button = -1;
+
+    /* Messages from the computer mark the beginning of a new
+     * question, so we have to send the NEW_QUESTION message
+     * to all the buttons */
+    Instruction inst(OP_NEW_QUESTION);
+    int msg_size = 0;
+    byte *msg = inst.to_message(&msg_size);
+
+    RF24NetworkHeader header(00, ID_INSTRUCTION);
+    network.multicast(header, msg, msg_size, 0);
+    network.multicast(header, msg, msg_size, 1);
+    delete[] msg;
+
+    /* send disable messages to the correct team */
+    int team_to_disable = (button_mask & 0x0F == 0x0F)? 0 : 1;
+    debug("button_mask = 0x%X, team_to_disable = %d", button_mask, team_to_disable);
+    msg_size = 0;
+    msg = NULL;
+    header = RF24NetworkHeader(00, ID_DISCONNECT);
+    network.multicast(header, msg, msg_size, team_to_disable);
+  }
+
+  /* Read each pairing button */
+  bool team_1_pair = digitalRead(PIN_PAIR_BTN_TEAM_1) == LOW,
+       team_2_pair = digitalRead(PIN_PAIR_BTN_TEAM_2) == LOW;
+
+  /* Enter pairing mode if pairing button is clicked */
+  if (team_1_pair && team_2_pair)
+  {
+    debug("Can't pair both teams simultaneously!");
+  }
+  else if (team_1_pair || team_2_pair)
+  {
+    in_pairing_mode = true;
+    pairing_mode_start_time = millis();
+    pairing_team = team_1_pair? 0 : 1;
+    debug("Entering pairing mode (Team %d)", pairing_team + 1);
+  }
+
+  /* Exit pairing mode after a certain timeframe */
+  if (in_pairing_mode
+      && millis() - pairing_mode_start_time > PAIRING_MODE_TIMEOUT)
+  {
+    in_pairing_mode = false;
+    debug("Leaving pairing mode");
+  }
+
+  delay(100);
+}
+
+
+/* digits[0]:  number
+ * digits[1]:  parent #
+ * digits[2]:  grandparent #
+ * digits[3]:  great-grandparent #
+ * 
+ * Returns the next available network address
+ *  eg. 02, 03, 04, 05, 11, 21, etc.
+ * 
+ * TODO: Clean this up?
+ * TODO: Document this better
+ *       (What is it doing? What is it needed for? etc.)
+ */
+uint16_t next_child(void)
+{
+  static uint8_t digits[4] = { 2, 0, 0, 0 };
+
+  uint16_t addr = digits[0];
+
+  if (digits[1] == 0)
+  {
+    addr = digits[0];
+  }
+  else if (digits[2] == 0)
+  {
+    addr =\
+        (digits[0] << 3)
+      |  digits[1];
+  }
+  else if (digits[3] == 0)
+  {
+    addr =\
+        (digits[0] << 6)
+      | (digits[1] << 3)
+      |  digits[2];
+  }
+  else
+  {
+    addr =\
+        (digits[0] << 9)
+      | (digits[1] << 6)
+      | (digits[2] << 3)
+      |  digits[3];
+  }
+
+  for (int i = 0; i < 4; ++i)
+  {
+    digits[i]++;
+    if (digits[i] > 5)
+    {
+      digits[i] = 1;
+    }
+    else
+    {
+      break;
+    }
+  }
+
+  return addr;
+}
+
+/* intepret_message: interpret a message from the network */
+void interpret_message(RF24NetworkHeader header, byte *message)
+{
+  switch(header.type)
+  {
+  /* Button event */
+  case ID_BUTTON_EVENT:
+   {
+    debug("Button Event");
+    //ButtonEvent e;
+
+    int team   = 0,
+        player = 0;
+
+    ///* Read the message into the event object */
+    //size_t offset = 1;
+    //memcpy (&e.time_ms, message + offset, sizeof(e.time_ms));
+    //offset += sizeof(e.time_ms);
+    //memcpy (&e.time_ms, message + offset, sizeof(e.time_us));
+
+    for (int t = 0; t < 2; ++t)
+    {
+      for (int i = 0; i < MAXIMUM_CHILDREN; ++i)
+      {
+        if (children[team][i] == header.from_node)
+        {
+          team = t;
+          player = i;
+          goto buttonevent_done_searching_for_child;
+        }
+      }
+    }
+buttonevent_done_searching_for_child:
+
+    uint8_t button_number = (team * 4) + player;
+
+    /* Make sure this button isn't disabled */
+    if (button_mask & ( 1 << button_number ) != 0)
+    {
+      /* We haven't sent an event to the computer
+       * yet, so this event will be sent */
+      if (pressed_button == -1)
+      {
+        pressed_button = button_number;
+        Serial.print(pressed_button);
+
+        /* Turn on the button's buzzer... */
+        Instruction inst(OP_WRITE_REGISTER);
+        inst.data[0] = REG_SOUND;
+        inst.data[1] = 0xFF;
+        int msg_size = 0;
+        byte *msg = inst.to_message(&msg_size);
+
+        RF24NetworkHeader header(children[team][player], ID_INSTRUCTION);
+        network.write(header, msg, msg_size);
+        delay(10);
+
+        /* ...For a certain amount of time */
+        inst = Instruction(OP_WRITE_REGISTER);
+        inst.data[0] = REG_SOUND_TIMER;
+        inst.data[1] = 10;
+
+        msg_size = 0;
+        msg = inst.to_message(&msg_size);
+    
+        header = RF24NetworkHeader(children[team][player], ID_INSTRUCTION);
+        network.write(header, msg, msg_size);
+        delete[] msg;
+      }
+    }
+
+    debug("Event from player #%d on team #%d.", player + 1, team + 1);
+    //debug("Event occurred at %lums, %luus, player #%d on team #%d.",
+    //      e.time_ms, e.time_us, player + 1, team + 1);
+   }break;
+
+  case ID_PING:
+   {
+    debug("Got '%s'", (char*)message);
+
+    RF24NetworkHeader response_header(header.from_node, ID_PING);
+    byte pong_msg[32] = "pong";
+    network.write(response_header, pong_msg, sizeof(pong_msg));
+    debug("Pong!");
+   }break;
+
+  case ID_PAIR:
+   {
+    debug("Pairing request received");
+    if (in_pairing_mode)
+    {
+      /* Make sure there's space on the team */
+      if (child_count[pairing_team] < MAXIMUM_CHILDREN)
+      {
+        uint16_t addr = next_child();
+
+        RF24NetworkHeader response_header(01, ID_PAIR);
+        byte pair_msg[32] =\
+        {
+          (byte)( addr        & 0xFF),
+          (byte)((addr >> 8)  & 0xFF),
+          (byte)(pairing_team & 0xFF),
+        };
+        network.write(response_header, pair_msg, sizeof(pair_msg));
+
+        children[pairing_team][child_count[pairing_team]++] = addr;
+
+        debug("New child is @ %d, on team %d", addr, pairing_team + 1);
+      }
+      else
+      {
+        /* Send a disconnect message back to the button to say we can't pair */
+        RF24NetworkHeader response_header(header.from_node, ID_DISCONNECT);
+        network.write(response_header, NULL, 0);
+        error("Can't pair -- team %d is full!", pairing_team + 1);
+      }
+    }
+    else
+    {
+      /* Send a disconnect message back to the button to say we can't pair */
+      RF24NetworkHeader response_header(header.from_node, ID_DISCONNECT);
+      network.write(response_header, NULL, 0);
+      error("Can't pair -- not in pairing mode!");
+    }
+   }break;
+
+  case ID_DISCONNECT:
+   {
+    debug("Disconnect message received");
+
+    /* Get the address to remove */
+    uint16_t discon_addr = 0;
+    memcpy(&discon_addr, message, sizeof(discon_addr));
+
+    debug("address to remove is %d", discon_addr);
+
+    /* Try to remove the child */
+    for (int team = 0; team < 2; ++team)
+    {
+      for (int i = 0; i < child_count[team]; ++i)
+      {
+        if (children[team][i] == discon_addr)
+        {
+          debug("removing child #%d", discon_addr);
+          children[team][i] = 0;
+  
+          memmove(children[team] + i , children[team] + i + 1, child_count[team] - i - 1);
+          child_count[team]--;
+          goto disconnect_done_searching_for_child;
+        }
+      }
+    }
+disconnect_done_searching_for_child:
+    ;
+   }break;
+
+  default:
+    error("Unknown or invalid message type 0x%.2hhX", header.type);
+    break;
+  }
+}
+
